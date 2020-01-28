38c8ad65285ebf938fb0ff7536dd862452c37751
janvisharma <janvi@connect.hku.hk>
2020-01-28 16:55:28 +0800

    Hello World

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://react-icons.netlify.com/static/media/react-icons.2e67b430.svg" className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

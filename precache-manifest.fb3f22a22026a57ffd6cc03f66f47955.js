e13d0af19436b346832a99c35bfd67b32be410d1
janvisharma <janvi@connect.hku.hk>
2020-01-29 14:42:06 +0800

    Updates

---
+++ precache-manifest.fb3f22a22026a57ffd6cc03f66f47955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c1a431e86c9de6366494890e63d383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71aa6956b9299bc8d662",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6d7b68e75e1300e81b4e",
+    "url": "/static/js/2.8a0b4a72.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8a0b4a72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71aa6956b9299bc8d662",
+    "url": "/static/js/main.aaafbaac.chunk.js"
+  },
+  {
+    "revision": "e06c96b9be6927f5c0c3",
+    "url": "/static/js/runtime-main.10bc7ff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

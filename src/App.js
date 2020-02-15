fbc5da45b9728fded4340c60eef8d024f5cfbeca
janvisharma <janvi@connect.hku.hk>
2020-02-15 22:52:02 +0530

    removes unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Demo from './Components/cardHolder';
-import { Divider } from '@material-ui/core';
+
 
 class App extends React.Component {
     render(){

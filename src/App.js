156613b103014b93df21ebd1d664688680abbd12
janvisharma <janvi@connect.hku.hk>
2020-02-15 22:43:29 +0530

    adds app.js and renders card component in it

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './Components/cardHolder';
+import { Divider } from '@material-ui/core';
+
+class App extends React.Component {
+    render(){
+        return(
+            <Demo />   
+        );
+    }
+}
+
+export default App;
+

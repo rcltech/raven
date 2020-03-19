41c2c1bdd4bd3bd4057ec75c700381f524de277d
seanchok <seanchok@gmail.com>
2020-03-19 17:47:02 +0800

    #15: build profile picture using media card

--- src/components/routes/user-profile/Profile.js
+++ src/components/routes/user-profile/Profile.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 400
+  }
+});
 
 export const Profile = ({ me }) => {
-  return <>{me.username}</>;
+  const classes = useStyles;
+
+  const { username, first_name, last_name, image_url } = me;
+
+  const imageSize = 220;
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          style={{ height: imageSize, width: imageSize }}
+          component="img"
+          src={image_url}
+          title="Profile picture"
+        />
+        <CardContent>
+          <Typography variant="h6">{`${first_name} ${last_name}`}</Typography>
+          <Typography variant="body1" color="textSecondary">
+            {username}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };

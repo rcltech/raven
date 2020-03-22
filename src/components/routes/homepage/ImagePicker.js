bb4a7a8822140aa450dcc07a49aa8acf705e18e9
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-03-22 15:49:09 +0800

    Fixes 404 error because of index.css in public folder (#20)
    * Fixes 404 error because of index.css in public folder
    
    * Changes text on upload-image button
    
    * Handles on image input change

--- src/components/routes/homepage/ImagePicker.js
+++ src/components/routes/homepage/ImagePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, FormLabel, makeStyles } from '@material-ui/core';
 import { CloudUpload } from '@material-ui/icons';
 
@@ -12,7 +12,17 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export const ImagePicker = ({ imageBase64, setImageBase64 }) => {
+  const [isImageUploaded, setImageUploaded] = useState(false);
   const classes = useStyles();
+
+  const handleOnImageInputChange = files => {
+    const imageFile = files[0];
+    if (!imageFile) return;
+    const fReader = new FileReader();
+    fReader.readAsDataURL(imageFile);
+    fReader.onloadend = ev => setImageBase64(ev.target.result);
+    if (!isImageUploaded) setImageUploaded(true);
+  };
 
   return (
     <Container className={classes.root}>
@@ -21,17 +31,11 @@ export const ImagePicker = ({ imageBase64, setImageBase64 }) => {
         className={classes.inputImage}
         id="event-image"
         type="file"
-        onChange={({ target: { files } }) => {
-          const imageFile = files[0];
-          if (!imageFile) return;
-          const fReader = new FileReader();
-          fReader.readAsDataURL(imageFile);
-          fReader.onloadend = ev => setImageBase64(ev.target.result);
-        }}
+        onChange={({ target: { files } }) => handleOnImageInputChange(files)}
       />
       <FormLabel htmlFor="event-image" className={classes.root}>
         <Button startIcon={<CloudUpload />} fullWidth={true} component="span">
-          {imageBase64 ? 'Change image' : 'Upload image'}
+          {isImageUploaded ? 'Change Image' : 'Upload Image'}
         </Button>
       </FormLabel>
     </Container>
